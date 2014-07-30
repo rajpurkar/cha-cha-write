d35ddb96b289b49a095fff70686ca0fd98c8ea76
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-07-30 13:16:27 -0700

    adds clippy

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.get('/', function(req, res){
+    res.render('main');
+});
+app.get('/main', function(req, res){
+    res.render('main');
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
