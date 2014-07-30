e0423740a9ef377871bd8f63ea1f2fad1ad94709
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-07-30 14:37:13 -0700

    simplifies clippy code

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-
 var app = express();
 
 // view engine setup
