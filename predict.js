e5c9c1eccffc11f85e91a16a9827bc8f36183380
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-08-12 10:26:56 -0700

    moves to preprocessing

--- predict.js
+++ predict.js
@@ -1,4 +1,5 @@
-var graph = require("./relations.json");
+var _ = require('underscore');
+var graph = require('./files/graph.json');
 
 function getRel(word, type){
 	var key = word + " " + type;
@@ -8,44 +9,3 @@ function getRel(word, type){
 }
 
 exports.getRel = getRel;
-
-function makeBirectional(){
-	for(var key in graph){
-		graph[key].forEach(function(value){
-			if(!(value in graph)){graph[value] = [];}
-			graph[value].push(key);
-		});
-	}	
-}
-
-function convertValToDict(){
-	for(var key in graph){
-		var values = graph[key];
-		var valuesD = {'v': {}, 'adj':{}, "o":{}};
-		values.forEach(function(value){
-			var parts = value.split(' ');
-			if(!(parts[0] in valuesD[parts[1]])){ valuesD[parts[1]][parts[0]] = 0;}
-			valuesD[parts[1]][parts[0]] += 1;
-		});
-		graph[key]= valuesD;
-	}	
-}
-
-function sortByOccurence(){
-	for(var key in graph){
-		for(var type in graph[key]){
-			var unsorted = graph[key][type];
-			graph[key][type] = Object.keys(unsorted).sort(function(curKey,nextKey) {
-	        	return unsorted[curKey] < unsorted[nextKey];
-	    	});
-		}
-		 
-	}
-}
-
-makeBirectional();
-convertValToDict();
-sortByOccurence();
-
-console.log(getRel("walk", "v"));
-//console.log(graph);
