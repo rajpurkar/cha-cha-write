9c0f430367ab406b04f5434451d7890f662c7095
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-08-12 17:21:44 -0700

    removes unnecessary addons

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,6 @@
 (function($){
+	//localstorage functions
+
 	function supportsLocalStorage() {
     return ('localStorage' in window) && window['localStorage'] !== null;
 	}
@@ -17,40 +19,15 @@
 		return true;
 	}
 
-	function updateSoFar(data){
-		if(data == undefined){ return; }
-		$("#soFar-text").empty();
-		var start = data.length - 5;
-		if (start < 0){ start = 0; }
-		//start = 0
-		for(var i = start; i < data.length; i++){
-			$("#soFar-text").append("<h3 class='soFar'>" + String(data[i]).split(",").join(" ") + "</h3>");
-		};
-	}
-
+	//processes the current state of the doc
 	function processInput(){
-		var presentText = $("#doc").text().replace(",", ".");
+		var presentText = $("#doc").text().replace(",", ".").split('.').splice(-5); //use the last 5 'phrases'
+		
 		$.get( "/predict/" + presentText, function(data){
-			if(data == undefined) return;
-			updateSoFar(data.soFar)
-			$("#next-text").empty();
-			data = data.predictions;
-			var max = data.length;
-			if(max > 5) { max = 5}
-			for(var k = 0; k < max; k++){
-				var kth = data[k];
-				$("#next-text").append("<h3 class='next'>" + String(kth.action) + "</h3>");
-			}
-		});
-	}
-
-
-	function getRandom(){
-		$.get('/randomv/10', function(data){
-			var re = new RegExp(',', 'g');
-			data = data.toString().replace(re, ' ');
-			console.log(data);
-			$("#next-text-new").text(data);
+			var graph = new Graph(data);
+			graph.convertGraph();
+			graph.draw();
+			//$('#sidebar-text').text(JSON.stringify(data).splice(10));
 		});
 	}
 
@@ -58,25 +35,15 @@
 		console.log(e.keyCode);
 		saveState();
 		if(e.keyCode === 46 || e.keyCode === 44){
-			getRandom();
+			//processInput();
 		}
 	});
-	
-	function getPredictions(link){
-		$.get(link, function(data){
-			data = JSON.parse(data);
-			$("#next-text").empty();
-			for(var k = 0; k < data.length; k++){
-				var kth = data[k];
-				$("#next-text").append("<h3 class='next'>" + String(kth.v2 + ","  + kth.o2).split(",").join(" ") + "</h3>");
-			}	
-		});	
-	}
 
 	if(resumeState()){
-		getRandom();
+		//getRandom();
 		//window.setInterval(getRandom, 15000);
-		//processInput();
+		//draw();
+		processInput();
     }
 })($);
 
