23e7e26d5639e95a6823b243d8c42250f36511fb
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-08-12 17:11:57 -0700

    moves modtype to predict.js

--- predict.js
+++ predict.js
@@ -1,11 +1,22 @@
 var _ = require('underscore');
 var graph = require('./files/graph.json');
 
-function getRel(word, type){
+exports.getRel = function(word, type){
 	var key = word + " " + type;
 	if(key in graph){
 		return graph[key];
 	} else return null;
 }
 
-exports.getRel = getRel;
+//change postag format
+exports.getModType = function(type){
+	var modtype = null;
+	if(type[0] === "V"){
+		modtype = 'v';
+	} else if (type[0] === "N"){
+		modtype = 'o';
+	} else if (type[0] === "A"){
+		modtype = 'adj';
+	}
+	return modtype;
+}
